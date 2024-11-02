d560c4d197ea897cf97420980d13c22ef74562d1
Dhanishwi Chalamalasetty <dhanishwichalamalasetty@dhanishwis-air.mynetworksettings.com>
2024-11-02 16:52:41 -0400

    added file

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,43 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">Dhanishwi Chalamalasetty </span>
+            from <span className="purple"> Buffalo,New York.</span>
+            <br />
+            I am recent graduate student from masters in data science at UB.
+            <br />
+            I have completed from my bachelors in computer science from JNTU-H.
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Playing Games
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Reading Tech Blogs
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+          </ul>
+
+          <p style={{ color: "rgb(155 126 172)" }}>
+            "Strive to build things that make a difference!"{" "}
+          </p>
+          <footer className="blockquote-footer">Dhanishwi</footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
